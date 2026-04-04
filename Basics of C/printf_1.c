0efa4434b3c1807bcb5cdbf9be858c9031558bb4
harshpatel2405 <harshpatel4504@gmail.com>
2026-04-04 16:56:39 +0530

    printf and printfPractice

---
+++ Basics of C/printf_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h> // header file
+/*
+returnType functionName()
+{
+
+}
+*/
+
+// Escape Sequence 
+/*
+    IT WILL USED INSIDE DOUBLE QUOTES ONLY
+    ANYWHERE AND ANYTIME 
+    \n - NEW LINE CHARACTER 
+    \t - TAB LINE CHARACTER 
+*/
+
+int main() // main function section
+{          // body of the function
+
+    printf("Hello\nWorld..."); // function call
+    printf("\nNamaste Duniya");
+    printf("\tHello Duniya..");
+    return 0;
+}
